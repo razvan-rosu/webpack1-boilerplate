27bf5d3103217e5cb71cf5209cab7ed3c4b56933
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-10 01:14:22 +0300

    Load fonts with existing url loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CommonsChunkPlugin = require('./node_modules/webpack/lib/optimize/CommonsChunkPlugin');
 var UglifyJsPlugin = require('./node_modules/webpack/lib/optimize/UglifyJsPlugin');
 
@@ -32,7 +32,18 @@ module.exports = {
             },
             {
                 test: /\.(png|jp(e*)g|svg)$/,
-                loader: 'url-loader?limit=900000'
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+                use: {
+                    loader: 'url-loader',
+                        options: {
+                        limit: 100000,
+                        mimetype: 'application/font-woff',
+                        name: "./src/fonts/[name].[ext]"
+                    },
+                }
             },
             {
                 test: /\.scss$/,
@@ -42,8 +53,8 @@ module.exports = {
     },
     devtool: 'source-map',
     plugins: [
-        new ExtractTextPlugin("styles.bundle.css"),
-        new webpack.optimize.CommonsChunkPlugin( { name: "vendor", filename: "vendor.bundle.js", minChunks: Infinity }),
+        new ExtractTextPlugin('styles.bundle.css'),
+        new webpack.optimize.CommonsChunkPlugin( { name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),
         new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, drop_console: false } })
     ]
 };
