3c9ea49f2011212582295766e95638e6ce5fe6bc
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-09 03:23:10 +0300

    Import and inject images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 require('../styles/master.scss');
 
-alert('Hello index.js');
+import babelimg from '../images/babel.png';
+import sassimg from '../images/sass.png';
+import webpackimg from '../images/webpack.png';
+import postcssimg from '../images/postcss-autoprefixer.png';
+// pattern is used in index.scss
+import pattern from '../images/pattern.png';
+
+const babel = document.getElementById('babel-img');
+if (babel) babel.src = babelimg;
+
+const sass = document.getElementById('sass-img');
+if (sass) sass.src = sassimg;
+
+const webpack = document.getElementById('webpack-img');
+if (webpack) webpack.src = webpackimg;
+
+const postcss = document.getElementById('postcss-img');
+if (postcss) postcss.src = postcssimg;
+
+// alert('Hello index.js');
