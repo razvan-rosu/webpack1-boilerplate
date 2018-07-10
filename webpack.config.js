28ef15ddd8cb3c2592e79aa3590126dd1ce9ea71
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-11 02:41:32 +0300

    Different build config based on ENV var

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
     },
     devServer: {
 	    inline: true,
-        hot: true,
         port: 3000
     },
     module: {
@@ -44,17 +43,37 @@ module.exports = {
                         name: "./src/fonts/[name].[ext]"
                     }
                 }
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style', 'css-loader?sourceMap!autoprefixer?browsers=last 2 versions!sass?sourceMap')
             }
         ]
     },
-    devtool: 'source-map',
     plugins: [
         new ExtractTextPlugin('styles.bundle.css'),
-        new webpack.optimize.CommonsChunkPlugin( { name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),
-        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, drop_console: false } })
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),    
     ]
 };
+
+// source maps on DEV or STAGE
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'stage') {
+    module.exports.devtool = 'source-map';
+    
+    module.exports.module.loaders.push(
+        {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style', 'css-loader?sourceMap!autoprefixer?browsers=last 2 versions!sass?sourceMap')
+        }
+    )
+} else {
+    module.exports.module.loaders.push(
+        {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style', 'css-loader!autoprefixer?browsers=last 2 versions!sass')
+        }
+    )
+}
+
+// minify on STAGE or PROD
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, drop_console: false } })
+    )
+}
