c806916a1a037d22a121ea1136c961844c40e4bf
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-11 02:58:53 +0300

    Import and execute lodash functions

--- src/scripts/page2.js
+++ src/scripts/page2.js
@@ -1,1 +1,16 @@
-alert('Hello page2.js');
+import sortBy from 'lodash/sortBy';
+import findIndex from 'lodash/findIndex';
+
+const users = [
+    { 'user': 'fred',   'age': 48 },
+    { 'user': 'barney', 'age': 36 },
+    { 'user': 'fred',   'age': 40 },
+    { 'user': 'barney', 'age': 34 }
+];
+
+const index = findIndex(users, { 'user': 'fred',   'age': 40 });
+const sortedArr = sortBy(users, ['user', 'age']);
+
+console.log('Hello page1.js');
+console.log('^^^', index);
+console.log('>>>', sortedArr);
