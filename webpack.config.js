a98a09654fd2717fa0c8f98b7f8640ba94ecef67
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-11 05:21:46 +0300

    Build folder sanitization for future hashed bundles

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CommonsChunkPlugin = require('./node_modules/webpack/lib/optimize/CommonsChunkPlugin');
 var UglifyJsPlugin = require('./node_modules/webpack/lib/optimize/UglifyJsPlugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -48,7 +49,16 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin('styles.bundle.css'),
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),    
+        new webpack.optimize.CommonsChunkPlugin({ 
+            name: 'vendor', 
+            filename: 'vendor.bundle.js', 
+            minChunks: Infinity 
+        }),
+        new CleanWebpackPlugin(['build'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ]
 };
 
