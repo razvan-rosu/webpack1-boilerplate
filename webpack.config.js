faf796b1aaaf3709c69e404ed61453dd3953cdcd
Razvan Rosu <razvan.rosu89@gmail.com>
2018-07-24 01:20:14 +0300

    Reindent file; add performance bundle monitoring based on production flag

--- webpack.config.js
+++ webpack.config.js
@@ -13,88 +13,95 @@ var UglifyJsPlugin = require('./node_modules/webpack/lib/optimize/UglifyJsPlugin
 // dynamicly insert hashed bundles into page template
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-/** 
- * ENV variables 
+// bundle performance debugger
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+/**
+ * ENV variables
 */
 var env = process.env.NODE_ENV;
 var isProd = env === 'production';
 
 module.exports = {
 	entry: {
-        app: [
-            './src/styles/master.scss',
-            './src/scripts/index.js'
-        ],
-	    playground: './src/scripts/pages/playground.js',
-        spa: './src/scripts/pages/spa.js',
-        vendor: ['mithril', 'mithril-i18next']
-    },
+    app: [
+      './src/styles/master.scss',
+      './src/scripts/index.js'
+    ],
+    playground: './src/scripts/pages/playground.js',
+    spa: './src/scripts/pages/spa.js',
+    vendor: ['mithril']
+  },
 	output: {
-        path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'build'),
 		filename: '[name].bundle.[chunkhash:4].js'
-    },
-    devServer: {
-	    inline: true,
-        port: 3000
-    },
-    devtool: isProd ? false : 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.s[ac]ss$/,
-                loader: isProd ? ExtractTextPlugin.extract('style', 'css-loader!autoprefixer?browsers=last 2 versions!sass') : ExtractTextPlugin.extract('style', 'css-loader?sourceMap!autoprefixer?browsers=last 2 versions!sass?sourceMap')
-            },
-            {
-				test: /\.js$/,
-                loader: 'babel',
-				exclude: /node_modules/,
-                query: {
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/,
-                loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader',
-                use: {                
-                    options: {
-                        mimetype: 'application/font-woff',
-                        name: "./src/fonts/[name].[ext]"
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].min.[contenthash:4].css'),
-        new webpack.optimize.CommonsChunkPlugin({ 
-            name: 'vendor', 
-            filename: 'vendor.bundle.js', 
-            minChunks: Infinity 
-        }),
-        new webpack.optimize.UglifyJsPlugin({ 
-            compress: { 
-                warnings: false, 
-                drop_console: false 
-            } 
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.html'),
-            filename: 'index.html',
-            chunks: ['vendor', 'app']
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'playground.html'),
-            filename: 'src/pages/playground.html',
-            chunks: ['vendor', 'app', 'playground']
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'pages', 'spa.html'),
-            filename: 'src/pages/spa.html',
-            chunks: ['vendor', 'app', 'spa']
-        })
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  devtool: isProd ? false : 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.s[ac]ss$/,
+        loader: isProd ? ExtractTextPlugin.extract('style', 'css-loader!autoprefixer?browsers=last 2 versions!sass') : ExtractTextPlugin.extract('style', 'css-loader?sourceMap!autoprefixer?browsers=last 2 versions!sass?sourceMap')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        use: {
+          options: {
+            mimetype: 'application/font-woff',
+            name: "./src/fonts/[name].[ext]"
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].min.[contenthash:4].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minChunks: Infinity
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+          warnings: false,
+          drop_console: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+      filename: 'index.html',
+      chunks: ['vendor', 'app']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'playground.html'),
+      filename: 'src/pages/playground.html',
+      chunks: ['vendor', 'app', 'playground']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'spa.html'),
+      filename: 'src/pages/spa.html',
+      chunks: ['vendor', 'app', 'spa']
+    })
+  ]
 };
+
+// bundle performance visualizer
+!(isProd) ? module.exports.plugins.push(new BundleAnalyzerPlugin()) : ''
